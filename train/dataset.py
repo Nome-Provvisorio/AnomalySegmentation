d7dfea35488a6f98e70e3e375dd510a0b23ba47a
AlesoGio <77014401+AlesoGio@users.noreply.github.com>
2024-12-22 10:58:24 +0100

    Update dataset.py

--- train/dataset.py
+++ train/dataset.py
@@ -62,8 +62,8 @@ class VOC12(Dataset):
 class cityscapes(Dataset):
 
     def __init__(self, root, co_transform=None, subset='train'):
-        self.images_root = os.path.join(root, '')
-        self.labels_root = os.path.join(root, '')
+        self.images_root = os.path.join(root, 'leftImg8bit\\')
+        self.labels_root = os.path.join(root, 'gtFine\\')
         
         self.images_root += subset
         self.labels_root += subset
