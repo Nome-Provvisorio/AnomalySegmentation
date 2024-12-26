aa3c3a6f525079c3da6c16528da79a318dad6b81
AlesoGio <77014401+AlesoGio@users.noreply.github.com>
2024-12-26 18:29:31 +0100

    Update main.py

--- imagenet/main.py
+++ imagenet/main.py
@@ -262,7 +262,7 @@ def validate(val_loader, model, criterion):
 
 
 
-def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+def save_checkpoint(state, is_best, filename='checkpoint_pretrain.pth.tar'):
     torch.save(state, filename)
     if is_best:
         shutil.copyfile(filename, 'model_best.pth.tar')
