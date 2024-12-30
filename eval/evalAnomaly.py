fcd469a80d69f22359467a8b52ab9eb2686c83fd
AlesoGio <77014401+AlesoGio@users.noreply.github.com>
2024-12-30 11:59:33 +0100

    Update evalAnomaly.py

--- eval/evalAnomaly.py
+++ eval/evalAnomaly.py
@@ -97,16 +97,19 @@ def main():
         # Seleziona la metrica basata sull'argomento
         if args.metric == "msp":
             # MSP
+            print("Metrica: MSP")
             anomaly_result = 1.0 - np.max(result.squeeze(0).data.cpu().numpy(), axis=0)
         
         elif args.metric == "maxentropy":
             # MAXENTROPY
+            print("Metrica: Max Entropy")
             probabilities = torch.softmax(result.squeeze(0), dim=0).data.cpu().numpy()
             entropy = -np.sum(probabilities * np.log(probabilities + 1e-12), axis=0)  # Evita log(0) con epsilon
             anomaly_result = entropy
         
         elif args.metric == "maxlogit":
             # MAXLOGIT
+            print("Metrica: Max Logit")
             anomaly_result = 1.0 - np.max(result.squeeze(0).data.cpu().numpy(), axis=0)
 
         #print("Parent: ", path.parent.parent)
