0559123c6c28d0fd5e7bc0a64d714472958fe5a4
vcata <ing.v.catalano@gmail.com>
2025-01-14 14:24:05 +0100

    change

--- train/main_erfnet_E+D.py
+++ train/main_erfnet_E+D.py
@@ -212,9 +212,9 @@ def train_model(model, train_loader, val_loader, optimizer, criterion1, criterio
                 # calculate iou
                 preds = outputs.argmax(dim=1)
                 total_iou += calculate_iou(preds, targets, NUM_CLASSES).mean().item()
-        val_miou.append(total_iou / len(val_loader))
+        val_miou.append(total_iou)
         average_epoch_loss_val = sum(epoch_loss_val) / len(epoch_loss_val)
-        print(f"Validation Mean IoU: {total_iou / len(val_loader)}")
+        print(f"Validation Mean IoU: {total_iou}")
         print(f"Average epoch loss: {average_epoch_loss_val}")
     if 1 == 1:
         print("----- FACCIO IL GRAFICO--------")
